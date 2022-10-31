a025ff80b131f8f7ecad14ad07fc329e10c26d7a
charlie <76509903+charlie0904@users.noreply.github.com>
2022-10-31 13:50:27 +0800

    Create nmaptonikto.sh

---
+++ nmaptonikto.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ports="80 443"
+curdate=$(date +%Y%m%d)
+nmap —n -Pn -iL targets.txt -oA $curdate\_nmap_tcp —-reason
+for testport in $ports
+do for targetip in $(awk '/'$testport'\/open/{print $2}' $curdate\_nmap_tcp.gnmap)
+do nikto -host $targetip:$testport -ask no —nointeractive -useragent "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1" -Format htm -output
+$curdate\_nikto_$targetip\_$testport.html
+done
+done
